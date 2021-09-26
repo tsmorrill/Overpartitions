4a1ba07aaf025954b121ac1b0dd6e8a8b48ed9fc
Thomas Morrill <morrill.math@gmail.com>
2021-09-26 09:36:53 -0400

    clean up class definition

--- code/Python/overpartition.py
+++ code/Python/overpartition.py
@@ -6,41 +6,65 @@ import math
 class Overpartition:
 
     def __init__(self, ov, nov):
+        self.ov = ov
+        self.nov = nov
 
+    def str(self):
+        ov, nov = list(self.ov), list(self.nov)
+        part_list = ov + nov
+        part_list.sort(reverse = True)
+        for part in ov:
+            part_list[part_list.index(part)] = "/" + str(part)
+        string = " + ".join([str(item) for item in part_list])
+        return(string)
+
+    def weight(self):
+        return(np.sum(self.ov) + np.sum(self.nov))
+
+    def max(self):
+        if self.ov.size == 0:
+            ov_max = 0
+        else:
+            ov_max = self.ov[0]
+        if self.nov.size == 0:
+            nov_max = 0
+        else:
+            nov_max = self.nov[0]
+        return(max(ov_max, nov_max))
+
+    def len(self):
+        return(self.ov.size + self.nov.size)
+
+    def d_rank(self):
+        return(self.max() - self.len())
+
+    def res1crank(self):
+        w1 = np.count_nonzero(self.nov == 1)
+        m1 = np.count_nonzero(self.nov > w1)
+        if self.len == 0:
+            res1crank = 0
+        elif w1 == 0:
+            res1crank = max(self.nov)
+        elif w1 == 1 and m1 == 0:
+            res1crank = 1
+        else:
+            res1crank = m1 - w1
+        return(res1crank)
+
+    @classmethod
+    def from_list_pair(cls, ov, nov):
         ov = list(set(ov))
         ov.sort(reverse=True)
         ov = np.array(ov)
         nov.sort(reverse=True)
         nov = np.array(nov)
+        return cls(ov, nov)
 
-        self.str = " + ".join(["/"+str(i) for i in ov] + [str(i) for i in nov])
-
-        self.ov = ov
-        self.nov = nov
-        self.weight = np.sum(ov) + np.sum(nov)
-
-        if ov.size == 0:
-            self.ov_max = 0
-        else:
-            self.ov_max = ov[0]
-        if nov.size == 0:
-            self.nov_max = 0
-        else:
-            self.nov_max = nov[0]
-        self.max = max(self.ov_max, self.nov_max)
-        self.len = ov.size + nov.size
-
-        self.d_rank = self.max - self.len
-        w1 = np.count_nonzero(nov == 1)
-        m1 = np.count_nonzero(nov > w1)
-        if self.len == 0:
-            self.res1crank = 0 
-        elif w1 == 0:
-            self.res1crank = max(nov)
-        elif w1 == 1 and m1 == 0:
-            self.res1crank = 1
-        else:
-            self.res1crank = m1 - w1
+a = Overpartition.from_list_pair([3,1],[1,2])
+print(a.str())
+print(a.weight())
+print(a.d_rank())
+print(a.res1crank())
 
 def partitions_distinct_bounded(n, max):
     list = []
