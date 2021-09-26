4dc7f77a2b630e58f28eac5499aa7e2066ebdeed
Thomas Morrill <morrill.math@gmail.com>
2021-09-26 11:50:42 -0400

    futzing with import (incomplete)

--- optn/optn.py
+++ optn/optn.py
@@ -121,13 +121,3 @@ class Overpartition:
             else:
                 nov.append(part)
         return cls.from_list_pair(ov, nov)
-
-a = Overpartition.from_neg_list([9,-9, 5, 4, 2, 2, -1])
-print(a)
-print("weight: {}".format(a.weight()))
-print("Dyson rank: {}".format(a.d_rank()))
-print("first residual crank: {}".format(a.res1crank()))
-f = a.frob_rep_1()
-print("First Frobenius representation:")
-print(f[0].str(separator=" "))
-print(f[1].str(separator=" "))
