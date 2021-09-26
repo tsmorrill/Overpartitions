1b3199a8471b1879b2274153ed346db1f391b2c9
Thomas Morrill <morrill.math@gmail.com>
2021-09-26 13:23:19 -0400

    moved example.py

---
+++ optn/example.py
@@ -0,0 +1,11 @@
+from optn import Overpartition
+
+a = Overpartition.from_neg_list([9,-9, 5, 4, 2, 2, -1])
+print(a)
+print("weight: {}".format(a.weight()))
+print("Dyson rank: {}".format(a.d_rank()))
+print("first residual crank: {}".format(a.res1crank()))
+f = a.frob_rep_1()
+print("first Frobenius representation:")
+print(f[0].str(separator=" "))
+print(f[1].str(separator=" "))
